1038cdec1b879a229b607067b003b7be1c9f379e
SetarehPishiyar <pishiyarsetareh@gmail.com>
2022-04-04 21:15:32 +0430

    changes in methods and constructor.

--- Jail.java
+++ Jail.java
@@ -1,21 +1,26 @@
 package MONOPOLY;
 public class Jail extends Square {
 
-
+    public Jail(int index){
+        super(index);
+    }
     public void sendToJail(Player jailedPlayer){
         jailedPlayer.inJail = true;
-        System.out.println("Player " + jailedPlayer.getName() + " is in jail now for the next 5 turns.");
+        System.out.println("Player " + jailedPlayer.getName() + " is in jail now.");
     }
     public void free(Player jailedPlayer){
         jailedPlayer.inJail = false;
         jailedPlayer.addMoney(-50);
-        jailedPlayer.turnsInJail = 0;
         System.out.println("Player " + jailedPlayer.getName() + " is free now.");
     }
     public void freeByTurns(Player jailedPlayer){
         jailedPlayer.inJail = false;
-        jailedPlayer.turnsInJail = 0;
         System.out.println("Player " + jailedPlayer.getName() + " is free now.");
     }
+
+    public void offer(Player currentPlayer){
+
+    }
+
 
 }
