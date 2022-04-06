290e742191f1a59e5f1d4f77ff3f2c1c160910b7
Melika Eghbal <100410032+MelEghbal@users.noreply.github.com>
2022-04-06 16:42:29 +0430

    Add files via upload
    Method offerBuying overrode.

--- Cinema.java
+++ Cinema.java
@@ -1,13 +1,30 @@
 package MONOPOLY;
+
+import java.util.Scanner;
+
 public class Cinema extends Property {
     public Cinema(int index) {
         super(index, 200, 25);
     }
 
-    //when owner buys a new cinema ( more than one ) this method should be called
+    //when owner buys a new cinema this method should be called
     public void increaseRent(Player currentPlayer) {
         if(currentPlayer.getNumCinemas() == 1)
             return;
         setRent(getRent()*2);
+    }
+
+    @Override
+    public void offerBuying(Property prop, Player currentPlayer){
+        if (!prop.mortgaged){
+            currentPlayer.doesntHaveEnoughMoney(currentPlayer, prop.price);
+            System.out.println("You can buy this cinema and earn money:) \n"+"Price:"+prop.price+"\n wanna buy this cinema? (enter buy if you want.)");
+            Scanner sc = new Scanner(System.in);
+            String answer = sc.next();
+            if (answer.equalsIgnoreCase("buy")){
+                currentPlayer.buyByProperty(prop);
+                increaseRent(currentPlayer);
+            }
+        }
     }
 }
