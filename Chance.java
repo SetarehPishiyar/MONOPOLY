587a06c3d70866f4f9d636f8a9a9a29207afcb54
SetarehPishiyar <pishiyarsetareh@gmail.com>
2022-04-10 02:44:15 +0430

    message added.

--- Chance.java
+++ Chance.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class Chance extends Square {
 
 
-    public Chance(int index){
-        super(index);}
+    public Chance(String name,int index){
+        super(name,index);}
 
     public void getChanceCard(Player currentPlayer, Board currentBoard) {
 
@@ -15,18 +15,28 @@ public class Chance extends Square {
         switch (RanNum) {
             case 1:
                 currentPlayer.money += 200;
+                System.out.println("You earned 200$.");
                 break;
             case 2:
                 currentBoard.jail.sendToJail(currentPlayer);
+                System.out.println("Go to Jail.");
                 break;
             case 3:
                 currentPlayer.money -= (currentPlayer.money / 10);
+                System.out.println("Pay tax.");
                 break;
             case 4:
                 currentPlayer.moveTo(index + 3);
+                System.out.println("move 3 squares forward.");
                 break;
             case 5:
-                currentBoard.jail.free(currentPlayer);
+                if(currentPlayer.inJail) {
+                    currentBoard.jail.free(currentPlayer);
+                    currentPlayer.money += 50;
+                    System.out.println("You are out of jail now");
+                }
+                else
+                    System.out.println("You are not in jail.");
                 break;
             case 6:
                 //Dafe baad Tax nadim  BONUS
